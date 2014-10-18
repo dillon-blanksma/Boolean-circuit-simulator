fbd74510e98debf5d90869f27531fa28e898c465
dillon-blanksma <dgblanks@gmail.com>
2014-10-18 14:29:52 -0700

    Create Circuit.java

---
+++ src/Circuit.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Circuit extends Executable
+{
+	private LinkedHashMap<String, String> circuitTree;
+	
+	public Circuit(CircuitParser cp, InputVectorParser ivp)
+	{
+		
+	}
+	
+	public LinkedHashMap<String, String> getCircuitTree()
+	{
+		return circuitTree;
+	}
+}
